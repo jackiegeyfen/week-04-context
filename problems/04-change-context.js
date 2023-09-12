04ac95977662402db597a08107d4f412aaf70894
EndlerGuppy <JenniferLaCoco@gmail.com>
2023-09-11 22:34:55 -0600

    p4

--- problems/04-change-context.js
+++ problems/04-change-context.js
@@ -1,6 +1,22 @@
 function changeContext(func, obj) {
-  // Your code here
+
+  return func.bind(obj)();
+
+  //return bindedFunc();
 }
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+function extractName() {
+  return this.name;
+}
+
+const kristen = new Person('Kristen');
+console.log(changeContext(extractName, kristen));
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
